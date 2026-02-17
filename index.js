ab32136211b78d259f7a653edcb1f4eaa9d3ed7c
thuomwangi <thuomwangi.a@gmail.com>
2026-02-17 14:04:19 +0300

    view req query

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const verifyToken = process.env.VERIFY_TOKEN;
 
 app.get('/', (req, res) => {
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
-  console.log(req.query)
+  console.log('This is the req JSON',req.query)
 
   if (mode === 'subscribe' && token === verifyToken) {
     console.log('WEBHOOK VERIFIED');
