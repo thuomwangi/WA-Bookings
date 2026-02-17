ec78c38af80d77302098ec5925595d60c098fd8b
thuomwangi <thuomwangi.a@gmail.com>
2026-02-17 18:05:30 +0300

    investigating the webhook body entry

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/', (req, res) => {
   const body = req.body;
   //only act if the message os from a real user.
   if (!req.body?.entry?.[0]?.changes?.[0]?.value?.messages?.[0]) {
-    console.log(req.body)
+    console.log(req.body.entry[0].changes[0].value)
     console.log('Not a real user message. --Ignoring');
     return;
   }
