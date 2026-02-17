bdf8be0c182facd83601e867450402eca25e6fa7
thuomwangi <thuomwangi.a@gmail.com>
2026-02-17 14:37:02 +0300

    debug

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
-  reply_action();
+  //reply_action();
   res.status(200).end();
 });
 
