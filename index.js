87007e67ce421f4867f8d167f55289202d9b1e12
thuomwangi <thuomwangi.a@gmail.com>
2026-02-17 18:28:51 +0300

    investigating list reply body

--- index.js
+++ index.js
@@ -24,14 +24,6 @@ app.get('/', (req, res) => {
   }
 });
 
-
-// app.post('/', (req, res) => {
-//   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
-//   console.log(`\n\nWebhook received ${timestamp}\n`);
-//   console.log(JSON.stringify(req.body, null, 2));
-//   res.status(200).end();
-// });
-
 app.post('/', (req, res) => {
   res.status(200).end();
 
@@ -46,6 +38,8 @@ app.post('/', (req, res) => {
   const message = body.entry[0].changes[0].value.messages[0];
   const from = message.from;
   const text = message.text?.body || 'No text content';
+
+  console.log(req.body?.entry?.[0]?.changes?.[0]?.value);
 
   console.log(`Message from ${from}: ${text}`);
 
