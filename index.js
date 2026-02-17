c834c93e51db3898ede6a4a8f6c649398b222964
thuomwangi <thuomwangi.a@gmail.com>
2026-02-17 17:08:21 +0300

    auth testing

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
+console.log('Access Token:', `Bearer ${accessToken}`);  
   console.log(JSON.stringify(req.body, null, 2));
   reply_action();
   res.status(200).end();
